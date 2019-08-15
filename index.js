5d9459589f158313b167b3ddc9a9a865e71b50e2
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 14:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function takeANumber (line, name) {
   line.push(name);
   
-  console.log("Welcome," + name + ".You are number" +)
+  console.log("Welcome," + name + ".You are number" + line.length + "in line.")
+  
+  return "Welcome," + name + "You are number" + line.length + "in line."
