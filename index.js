c03c0a98398255e498d37db430cc9bbcc4b9395b
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 14:36:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function takeANumber (line, name) {
-  hiroDeli.push('${name}')
+  if (!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobodt waiting to be served!"
+  } else {
+    return "Currently serving" + line.shift
+  }
 }
