04e5b22fcd1de299ff7abe702b1b5fbad7313423
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 14:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ function takeANumber (line, name) {
   console.log("Welcome," + name + ".You are number" + line.length + "in line.")
   
   return "Welcome," + name + "You are number" + line.length + "in line."
+}
