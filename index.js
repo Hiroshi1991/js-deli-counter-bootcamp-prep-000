3dd3bf5bb9e8c6ed64ec15e7571f0fb4c498d99c
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 15:12:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(line) {
 
 function currentLine(line){
   if(!line.length) {
-    return "The Line is currently emplty.";
+    return "The Line is currently empty.";
   }
   
 }
