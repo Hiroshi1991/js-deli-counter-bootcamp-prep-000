eb75cb39eb87db829889670f0c606aa213cf9f55
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 14:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nowServing(line) {
   if(!line.length) {
     console.log("There is nobody waiting to be served!")
   } else {
-    return "Currently serving" + line.shift();
+    return "Currently serving " + line.shift();
   }
 }
